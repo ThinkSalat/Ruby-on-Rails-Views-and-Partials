af7ee5972ecdc3614100e6158c6f8021eec9136a
Shawn Salat <Shawn.Salat@Gmail.com>
2018-05-08 18:23:03 -0700

    Fix bug in new.html.erb

--- ninetyninecats/app/controllers/cat_rental_requests_controller.rb
+++ ninetyninecats/app/controllers/cat_rental_requests_controller.rb
@@ -5,12 +5,12 @@ class CatRentalRequestsController < ApplicationController
   end
 
   def new
-    @new = CatRentalRequest.new
+    @crr = CatRentalRequest.new
     render :new
   end
 
   def edit
-    @edit = CatRentalRequest.find(params[:id])
+    @crr = CatRentalRequest.find(params[:id])
     render :edit
   end
 
@@ -20,9 +20,25 @@ class CatRentalRequestsController < ApplicationController
   end
 
   def update
+    @crr = CatRentalRequest.find(params[:id])
+    if @crr.update(crr_params)
+      redirect_to cat_rental_request_url(@crr)
+    else
+      render :edit
+    end
   end
 
   def create
+    @crr = CatRentalRequest.new(crr_params)
+    if @crr.save
+      redirect_to cat_url(@crr.cat_id)
+    else
+      render json: @crr.errors.full_messages
+    end
   end
 
+  private
+  def crr_params
+    params.require(:cat_rental).permit(:cat_id,:start_date,:end_date)
+  end
 end
